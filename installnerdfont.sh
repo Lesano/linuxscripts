4b96016212cfc35e9dd1b731d4cd325c9b85a103
Lesano Tsuki <47015479+Lesano@users.noreply.github.com>
2024-05-11 01:05:17 +0100

    Add Installer Nerd Font for Arch

---
+++ installnerdfont.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "1) Installing unzip from pacman..."
+sudo pacman -S unzip --noconfirm
+echo "2) Downloading FiraCode from nerdfonts.com"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+echo "3) Unziping FiraCode..."
+unzip FiraCode.zip -d firacode && cd firacode
+echo "4) Moving all fonts..."
+sudo mv *.ttf /usr/share/fonts
+cd ..
+echo "5) Cleaning up..."
+rm -rf firacode && rm -rf Firacode.zip
+echo "6) Done!"
